b4ace20961a02fbd6944a622a8466befc14c8d94
Lars Eckart <lars.eckart@hey.com>
2022-01-17 11:37:49 +0200

    close instructions

--- app/src/main/java/com/larseckart/wordle/App.java
+++ app/src/main/java/com/larseckart/wordle/App.java
@@ -12,6 +12,7 @@ public class App {
             Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
             Page page = browser.newPage();
             page.navigate("https://www.powerlanguage.co.uk/wordle/");
+            page.click("game-modal path");
 
             // Pause on the following line.
             page.pause();
