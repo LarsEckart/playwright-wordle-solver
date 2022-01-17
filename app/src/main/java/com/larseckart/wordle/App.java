42bc09d16293bec3c79452da7fd22dbf40992b78
Lars Eckart <lars.eckart@hey.com>
2022-01-17 12:11:15 +0200

    guess a word

--- app/src/main/java/com/larseckart/wordle/App.java
+++ app/src/main/java/com/larseckart/wordle/App.java
@@ -7,15 +7,24 @@ import com.microsoft.playwright.Playwright;
 
 public class App {
 
-    public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
-            Page page = browser.newPage();
-            page.navigate("https://www.powerlanguage.co.uk/wordle/");
-            page.click("game-modal path");
+  public static void main(String[] args) {
+    try (Playwright playwright = Playwright.create()) {
+      Browser browser = playwright.firefox()
+          .launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+      Page page = browser.newPage();
+      page.navigate("https://www.powerlanguage.co.uk/wordle/");
+      page.click("game-modal path");
 
-            // Pause on the following line.
-            page.pause();
-        }
+      page.click("button:has-text(\"w\")");
+      page.click("button:has-text(\"o\")");
+      page.click("button:has-text(\"r\")");
+      page.click("button:has-text(\"l\")");
+      page.click("button:has-text(\"d\")");
+
+      page.click("text=enter");
+
+      // Pause on the following line.
+      page.pause();
     }
+  }
 }
